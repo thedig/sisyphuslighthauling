661cb5d72b6b7d55d8b826c9b87d783aa8ea4423
Dig <dig.s.vargas@gmail.com>
2014-02-01 12:44:12 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClairePortfolio::Application.config.secret_token = 'a45064ec4f560c1b2701979dbdfbebbb880f5836f7d447cb6de89dca280ad0337a28d5af2294f1fd4291471a043acfed3892b92a14a62d5c158890c3abdc5849'
